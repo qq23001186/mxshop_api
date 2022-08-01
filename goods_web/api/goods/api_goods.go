0fd08eddc1085745b40635a3b34bc2f464d84d6f
qq23001186 <qq23001186@163.com>
2022-08-01 23:58:54 +0800

    ver5.51 - implement api_goods : List

--- goods_web/api/goods/api_goods.go
+++ goods_web/api/goods/api_goods.go
@@ -1,14 +1,18 @@
 package goods
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"net/http"
+	"strconv"
 	"strings"
 	"web_api/goods_web/global"
+	"web_api/goods_web/proto"
 )
 
 func HandleGrpcErrorToHttp(err error, c *gin.Context) {
@@ -64,5 +68,81 @@ func removeTopStruct(fields map[string]string) map[string]string {
 
 func List(ctx *gin.Context) {
 	fmt.Println("商品列表")
+	//商品的列表
+	//////////////////////////////////////////// 根据前端的请求参数构造request对象
+	request := &proto.GoodsFilterRequest{}
+	priceMin := ctx.DefaultQuery("pmin", "0")
+	priceMinInt, _ := strconv.Atoi(priceMin)
+	request.PriceMin = int32(priceMinInt)
+	priceMax := ctx.DefaultQuery("pmax", "0")
+	priceMaxInt, _ := strconv.Atoi(priceMax)
+	request.PriceMax = int32(priceMaxInt)
+	isHot := ctx.DefaultQuery("ih", "0")
+	if isHot == "1" {
+		request.IsHot = true
+	}
+	isNew := ctx.DefaultQuery("in", "0")
+	if isNew == "1" {
+		request.IsNew = true
+	}
+	isTab := ctx.DefaultQuery("it", "0")
+	if isTab == "1" {
+		request.IsTab = true
+	}
+	categoryId := ctx.DefaultQuery("c", "0")
+	categoryIdInt, _ := strconv.Atoi(categoryId)
+	request.TopCategory = int32(categoryIdInt)
+	pages := ctx.DefaultQuery("p", "0")
+	pagesInt, _ := strconv.Atoi(pages)
+	request.Pages = int32(pagesInt)
+	perNums := ctx.DefaultQuery("pnum", "0")
+	perNumsInt, _ := strconv.Atoi(perNums)
+	request.PagePerNums = int32(perNumsInt)
+	keywords := ctx.DefaultQuery("q", "")
+	request.KeyWords = keywords
+	brandId := ctx.DefaultQuery("b", "0")
+	brandIdInt, _ := strconv.Atoi(brandId)
+	request.Brand = int32(brandIdInt)
 
+	////////////////////////////////////////////请求商品的service服务
+	r, err := global.GoodsSrvClient.GoodsList(context.WithValue(context.Background(), "ginContext", ctx), request)
+	if err != nil {
+		zap.S().Errorw("[List] 查询 【商品列表】失败")
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	reMap := map[string]interface{}{
+		"total": r.Total,
+	}
+
+	goodsList := make([]interface{}, 0)
+	for _, value := range r.Data {
+		goodsList = append(goodsList, map[string]interface{}{
+			"id":          value.Id,
+			"name":        value.Name,
+			"goods_brief": value.GoodsBrief,
+			"desc":        value.GoodsDesc,
+			"ship_free":   value.ShipFree,
+			"images":      value.Images,
+			"desc_images": value.DescImages,
+			"front_image": value.GoodsFrontImage,
+			"shop_price":  value.ShopPrice,
+			"category": map[string]interface{}{
+				"id":   value.Category.Id,
+				"name": value.Category.Name,
+			},
+			"brand": map[string]interface{}{
+				"id":   value.Brand.Id,
+				"name": value.Brand.Name,
+				"logo": value.Brand.Logo,
+			},
+			"is_hot":  value.IsHot,
+			"is_new":  value.IsNew,
+			"on_sale": value.OnSale,
+		})
+	}
+	reMap["data"] = goodsList
+
+	ctx.JSON(http.StatusOK, reMap)
 }
